d415116514e0867c449fde957aaa33eb24a3ba18
ntp_vv <63070054@kmitl.ac.th>
2023-03-23 23:04:03 +0700

    Tets Button

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,10 +3,10 @@ import Button from "@mui/material/Button";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-6">
         <p className="font-bold text-xl">adsfadsfsad</p>
         <p>wave</p>
-        <Button>test</Button>
+        <Button color="primary" variant="contained">test</Button>
       </div>
     </>
   )
