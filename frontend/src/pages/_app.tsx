254a3178c8b14ef8048b350aa861f264c336c4b0
ntp_vv <63070054@kmitl.ac.th>
2023-03-23 23:18:07 +0700

    Init Kanit Font

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import AlreadySignInLayout from './../components/Layout/AlreadySignInLayout';
-import {createTheme} from "@mui/material";
-import {ThemeProvider} from '@mui/material/styles';
+import { createTheme } from "@mui/material";
+import { ThemeProvider } from '@mui/material/styles';
 import NotSignInLayout from '@/components/Layout/NotSignInLayout';
 import { useState } from 'react';
 
@@ -15,25 +15,27 @@ export default function App({ Component, pageProps }: AppProps) {
       secondary: {
         main: '#E3F0F0'
       },
-      info:{
-        main:'#FF7F56'
+      info: {
+        main: '#FF7F56'
       }
-      
-    }
+    },
+    typography: {
+      fontFamily: "Kanit",
+    },
   });
   const [isLogin, setIsLogin] = useState(false);
   return (
     <ThemeProvider theme={theme}>
-    {isLogin ? (
-    <AlreadySignInLayout>
-      <Component {...pageProps} />
-    </AlreadySignInLayout>
-    ) : (
-    <NotSignInLayout>
-      <Component {...pageProps} />
-    </NotSignInLayout>
-    )
-    }
+      {isLogin ? (
+        <AlreadySignInLayout>
+          <Component {...pageProps} />
+        </AlreadySignInLayout>
+      ) : (
+        <NotSignInLayout>
+          <Component {...pageProps} />
+        </NotSignInLayout>
+      )
+      }
     </ThemeProvider>
   )
 }
