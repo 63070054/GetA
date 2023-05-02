72d1131e7d6e0b6a795e7cb092cae8c89fef87fb
ntp_vv <63070054@kmitl.ac.th>
2023-05-03 02:32:02 +0700

    switch to production path

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  server: {
+    // host: "localhost",
+    host: "3.86.227.129",
+    port: 3000,
+  },
+};
 
-module.exports = nextConfig
-
+module.exports = nextConfig;
