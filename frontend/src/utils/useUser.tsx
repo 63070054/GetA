b3671ff24f4874d7e7eec573c783dc4bbb147cb3
kedmanee <63070012@kmitl.ac.th>
2023-05-03 10:56:58 +0700

    dfjalskjfladsjkfa;

--- frontend/src/utils/useUser.tsx
+++ frontend/src/utils/useUser.tsx
@@ -4,6 +4,7 @@ export const useUser = () => {
 
   const token = Cookies.get("token") || ""
   const decodedToken: User = token ? JSON.parse(Buffer.from(token, 'base64').toString('utf-8'))?.[0] : null;
+  console.log(decodedToken?.id)
   return {
     ...decodedToken
   };
